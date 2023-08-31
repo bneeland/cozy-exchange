1c0a68000fc64167396dbb1353fc01a4eb5b0c57
Brian Neeland <brian@neeland.org>
2023-08-30 19:34:20 -0600

    Make button allow for only icon or only label

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,7 +9,7 @@ export default function Button({
   type = 'button',
   href = '/',
 }: {
-  icon: ReactNode
+  icon?: ReactNode
   label?: string
   onClick?: any
   color?: 'dark' | 'light'
@@ -17,18 +17,27 @@ export default function Button({
   href?: string
 }) {
   const classNames = `
-    px-4 py-2 rounded-2xl border-b-4 active:border-b-0 active:border-t-4 border-slate-300 hover:border-slate-400 flex items-center gap-3 transition-colors
+    px-4 py-2 rounded-2xl border-b-4 active:border-b-0 active:border-t-4 border-slate-300 hover:border-slate-400 flex items-center gap-3
     ${color === 'dark' && 'bg-slate-200 hover:bg-slate-300'}
     ${color === 'light' && 'bg-white hover:bg-slate-300'}
+    ${(icon && !label || label && !icon) ? 'flex justify-center' : ''}
   `
+
+  function Content() {
+    return (
+      <>
+        {icon && <div className="text-slate-400">{icon}</div>}
+        {label && <div className="text-slate-800">{label}</div>}
+      </>
+    )
+  }
 
   if (type === 'button') return (
     <button
       className={classNames}
       onClick={onClick}
     >
-      <div className="text-slate-400">{icon}</div>
-      {label && <div className="text-slate-800">{label}</div>}
+      <Content />
     </button>
   )
   
@@ -38,8 +47,7 @@ export default function Button({
       onClick={onClick}
       href={href}
     >
-      <div className="text-slate-400">{icon}</div>
-      {label && <div className="text-slate-800">{label}</div>}
+      <Content />
     </Link>
   )
 
